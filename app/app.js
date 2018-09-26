980e5784934b7b64dbb746bfb265c676502c5ac8
OmarNizam <eng.omarnizam@gmail.com>
2018-09-26 18:28:45 +0200

    ng-repeat & ng-click

--- app/app.js
+++ app/app.js
@@ -4,4 +4,12 @@ var app = angular.module("myApp", []);
 
 app.controller("mainCtrl", function () {
     this.hello = "world";
+
+    this.fruits = ["Apple", "orange", "grapes"];
+
+    this.alertMe = function() {
+        alert("something went wrong !!!")
+    };
+
+
 });
