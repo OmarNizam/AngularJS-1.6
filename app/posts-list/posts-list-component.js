d3baec31756a7066fd815b794264231be67bcad6
OmarNizam <eng.omarnizam@gmail.com>
2018-09-27 17:02:56 +0200

    fix the component structure

--- app/posts-list/posts-list-component.js
+++ app/posts-list/posts-list-component.js
@@ -1,9 +1,18 @@
 "use strict";
 
 (function(){
-    var app = angular.module('postsListModule', []);
+    // each component has module
+    var posts = angular.module('postsListModule', []);
 
-    // first we write the component controller
+    // First declare the component (the name must be JS camal case)
+
+    posts.component('postsList', {
+        templateUrl: 'posts-list/posts-list.html',
+        controller: PostsListController,
+        controllerAs: 'vm'
+    })
+
+    // Second we write the component controller
     function PostsListController(mainSvc) {
         var vm = this;
         mainSvc.getPosts().then((response) => {
@@ -11,13 +20,5 @@
         })
 
     }
-
-    // Second declare the component (the name must be JS camal case)
-
-    app.component('postsList', {
-        templateUrl: 'posts-list/posts-list.html',
-        controller: PostsListController,
-        controllerAs: 'vm'
-    })
 
 })();
