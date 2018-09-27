2aa5c50f7a959d039c8353b318b3d543d7bb6995
OmarNizam <eng.omarnizam@gmail.com>
2018-09-27 13:26:24 +0200

    calles with example of getting posts

--- app/app.js
+++ app/app.js
@@ -1,8 +1,12 @@
 "use strict";
 
+// the app module
 var app = angular.module("myApp", []);
+// main controller
+app.controller("mainCtrl", function ($http) {
 
-app.controller("mainCtrl", function () {
+    let vm = this;
+
     this.hello = "world";
 
     this.fruits = ["Apple", "orange", "grape"];
@@ -11,10 +15,11 @@ app.controller("mainCtrl", function () {
         alert("something went wrong !!!")
     };
 
+    $http.get("https://jsonplaceholder.typicode.com/posts").then((response) => {
+        vm.posts = response.data;
+    })
 
 });
-
-
 // custom filter
 app.filter('makePlural', function(){
     return function(input) {
