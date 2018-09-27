ad48099fff6e643ef89f32842ff90ce2bb3575b7
OmarNizam <eng.omarnizam@gmail.com>
2018-09-27 12:18:05 +0200

    Search filter and custom filter

--- app/app.js
+++ app/app.js
@@ -5,11 +5,19 @@ var app = angular.module("myApp", []);
 app.controller("mainCtrl", function () {
     this.hello = "world";
 
-    this.fruits = ["Apple", "orange", "grapes"];
+    this.fruits = ["Apple", "orange", "grape"];
 
     this.alertMe = function() {
         alert("something went wrong !!!")
     };
 
 
+});
+
+
+// custom filter
+app.filter('makePlural', function(){
+    return function(input) {
+        return input + "s";
+    }
 });
